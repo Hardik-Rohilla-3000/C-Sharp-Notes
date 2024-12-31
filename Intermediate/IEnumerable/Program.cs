cc3fa5110d094524ba909b5eb80c111befa0de2c
Hardik-Rohilla-3000 <hardikrohilla12@gmail.com>
2024-12-31 20:54:00 +0530

    IEnumerable

---
+++ Intermediate/IEnumerable/Program.cs
@@ -0,0 +1,27 @@
+﻿// learn how to make your custom types work with foreach 
+namespace IEnumerable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        List<int> intList = new List<int>();
+        foreach (int i in intList){
+
+        }
+        Console.ReadKey();
+    }
+    public class PlayerStat{
+        public Stat dex = new Stat{name = "Dexterity", value = 12};
+        public Stat str = new Stat{name = "Strength", value = 28};
+        public Stat wis = new Stat{name = "Wisdom", value = 35};
+    }
+    public class Stat{
+        public string name;
+        public int value;
+    }
+    private class Player{
+        
+    }
+}
